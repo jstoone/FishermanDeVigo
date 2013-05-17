311e1a04d02016d5c2dba01dbc713734750ff7a4
Dylan Dhokia <dylandhokia@gmail.com>
2013-05-17 22:34:56 +0100

    Changed To Spanish Names

--- src/com/dddbomber/fishingjam/instance/family/Person.java
+++ src/com/dddbomber/fishingjam/instance/family/Person.java
@@ -4,9 +4,12 @@ import com.dddbomber.fishingjam.assets.NameGenerator;
 
 public class Person {
 	public int happiness = 100, health = 100, fatigue = 100, hunger = 100;
+	public boolean canWork, male;
 	public String name = "";
 	
-	public Person(){
+	public Person(boolean male, boolean canWork){
+		this.male = male;
+		this.canWork = canWork;
 		name = NameGenerator.getInstance().getName();
 	}
 	
