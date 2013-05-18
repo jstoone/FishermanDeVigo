cff0b59ad7b76b8f9e73ef51ddea3a291d57d0fa
Dylan Dhokia <dylandhokia@gmail.com>
2013-05-18 14:32:45 +0100

    Lots Of Stuff

--- src/com/dddbomber/fishingjam/instance/family/Person.java
+++ src/com/dddbomber/fishingjam/instance/family/Person.java
@@ -3,7 +3,7 @@ package com.dddbomber.fishingjam.instance.family;
 import com.dddbomber.fishingjam.assets.NameGenerator;
 
 public class Person {
-	public int happiness = 100, health = 100, fatigue = 100, hunger = 100;
+	public int happiness = 100, health = 100, hunger = 100;
 	public boolean canWork, male;
 	public String name = "";
 	
---
+++ src/com/dddbomber/fishingjam/menu/FamilyMenu.java
@@ -0,0 +1,64 @@
+package com.dddbomber.fishingjam.menu;
+
+import java.awt.event.KeyEvent;
+
+import com.dddbomber.fishingjam.assets.Asset;
+import com.dddbomber.fishingjam.assets.Screen;
+import com.dddbomber.fishingjam.input.InputHandler;
+import com.dddbomber.fishingjam.instance.Instance;
+import com.dddbomber.fishingjam.instance.family.Person;
+
+public class FamilyMenu extends Menu{
+
+	public boolean toggle = false;
+	
+	public void tick(InputHandler input) {
+		boolean up = input.keyboard.keys[KeyEvent.VK_UP];
+		boolean down = input.keyboard.keys[KeyEvent.VK_DOWN];
+		if(!up && !down)toggle = false;
+		if(toggle)return;
+		if(up || down){
+			toggle = true;
+			Person p = Instance.getInstance().family.get(selected);
+			if(p.name.length() == 0){
+				p.name = "Unnamed";
+			}
+		}
+		if(up){
+			if(selected > 0)selected--;
+		}else if(down){
+			if(selected < Instance.getInstance().family.size()-1)selected++;
+		}
+	}
+	
+	public int selected = 0;
+
+
+	public void keyTyped(KeyEvent e) {
+		Person p = Instance.getInstance().family.get(selected);
+		if(e.getKeyChar() != KeyEvent.VK_BACK_SPACE){
+			if(p.name.length() < 40)p.name += e.getKeyChar();
+		}else if(p.name.length() > 0){
+			p.name = p.name.substring(0, p.name.length()-1);
+		}
+	}
+
+	public void render(Screen screen, InputHandler input) {
+		screen.fill(0, 0, 600, 400, 0, 100);
+		for(int i = 0; i < Instance.getInstance().family.size(); i++){
+			Person p = Instance.getInstance().family.get(i);
+			if(i == selected)screen.fill(1, i*64+1, 598, 14, 0xbcbcbc, 100);
+			screen.draw(p.name, 2, i*64+2, 0xffffff, 2);
+			
+			screen.draw(Asset.counter, 2, i*64+20, (int)(8-p.health/12.5)*32, 0, 32, 32);
+			screen.draw("Health", 40, i*64+30, 0xffffff, 2);
+			
+			screen.draw(Asset.counter, 130, i*64+20, (int)(8-p.hunger/12.5)*32, 0, 32, 32);
+			screen.draw("Hunger", 168, i*64+30, 0xffffff, 2);
+			
+			screen.draw(Asset.counter, 258, i*64+20, (int)(8-p.happiness/12.5)*32, 0, 32, 32);
+			screen.draw("Happy", 296, i*64+30, 0xffffff, 2);
+		}
+	}
+
+}
